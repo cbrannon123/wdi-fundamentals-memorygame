a2486b6583fa92b2247a65d7ef4573bab096ff9b
Casey Brannon <caseybrannon@Caseys-MBP.fios-router.home>
2019-04-08 14:52:26 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = 'king';
+var cardFour = 'king';
+console.log('User Flipped ' + cardOne);
+console.log('User Flipped ' + cardFour);
+
+
+
+
