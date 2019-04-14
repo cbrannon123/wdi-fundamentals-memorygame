5eb9af35e598114c85b27871f7725da0d7a5463a
Casey Brannon <caseybrannon@Caseys-MBP.fios-router.home>
2019-04-13 17:57:41 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,8 +6,8 @@ var cards = [
 },
 {
 	rank: "queen",
-	suit: "dimonds",
-	cardImage: "images/queen-of-dimonds.png"
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
 },
 {
 	rank: "king",
@@ -16,8 +16,8 @@ var cards = [
 },
 {
 	rank: "king",
-	suit: "dimonds",
-	cardImage: "image/king-of-dimonds.png"
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
 }
 
 ];
@@ -27,21 +27,40 @@ var cardsInPlay = [];
 var checkForMatch = function() {
 	if (cardsInPlay.length === 2)
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-			console.log("You found a match");
+			alert("You found a match");
 		} else {
-			console.log("Sorry, try again");
+			alert("Sorry, try again");
 		}
 };
 
-var flipCard = function (cardId) {
+var flipCard = function (cardElement) {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push (cards[cardId].rank);
-	if (cardsInPlay.length === 2){
-	}		
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2){
 	checkForMatch();
+}
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function (){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);	
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+ }
+        
+};
+
+function reset() {
+	location.reload();
+};
+
+
+
+
+createBoard()
